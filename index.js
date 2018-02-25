4ad74fc8eea07f7000e2c11006ea6d20b6475600
Dallin <andersondallin11@gmail.com>
2018-02-24 17:47:32 -0700

    Started

---
+++ index.js
@@ -0,0 +1,8 @@
+function hideShow() {
+    var sh = document.getElementById("new-delivery-page");
+    if (sh.style.display === "none") {
+        sh.style.display = "block";
+    } else {
+        sh.style.display = "none";
+    }
+}
